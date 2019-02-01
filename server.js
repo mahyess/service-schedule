0fd598fec07001af71ca8de0a9a47ee3412147ed
zeph <zephyrr2722@gmail.com>
2019-02-01 19:14:00 +0545

    Server.js init

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+// const users = require('./routes/api/users');
+// const profile = require('./routes/api/profile');
+// const posts = require('./routes/api/posts');
+
+const app = express();
+
+// BodyParser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//DB config
+// const db = require('./config/keys').mongoURI;
+
+//connect to mongoDB
+// mongoose
+// 	.connect(db)
+// 	.then(() => console.log('mongoDB Connected'))
+// 	.catch(err => console.log(err));
+
+// passport middleware
+app.use(passport.initialize());
+
+// passport config
+// require('./config/passport')(passport);
+
+// use Routes
+// app.use('/api/users', users);
+// app.use('/api/profile', profile);
+// app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
